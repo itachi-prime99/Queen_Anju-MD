4cf9714c339b6d99039d3de4cbd5aa2e79e5b907
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-04-20 12:23:10 +0600

    Update queen.js

--- queen.js
+++ queen.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94717775628']
+const ownerNumber = ['8801839268235']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
